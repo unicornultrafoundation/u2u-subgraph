2f2653e0c68d1175f9b5876ebd0ef5c4de23e65b
phidangnguyen <phidang0210@gmail.com>
2023-08-27 08:22:47 +0700

    udelegate + withdraw event handle

--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,5 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+
 export function concatID(str1: string, str2: string): string {
   return str1.concat("-").concat(str2);
 }
@@ -5,3 +7,11 @@ export function concatID(str1: string, str2: string): string {
 export function isEqual(str1: string, str2: string): boolean {
   return str1.toLowerCase() == str2.toLowerCase();
 }
+
+export function arrayContained(arr: string[], val: string): boolean {
+  if (!arr || arr.length == 0) return false
+  return arr.indexOf(val) > -1;
+}
+
+export let ZERO_BI = BigInt.fromI32(0)
+export let EMPTY_STRING = "";
