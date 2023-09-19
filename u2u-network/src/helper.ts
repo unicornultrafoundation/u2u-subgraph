84df425085d850f78c2d728b0453d2a46db25908
phidangnguyen <phidang0210@gmail.com>
2023-09-20 00:19:18 +0700

    [WIP] burnt fees

--- u2u-network/src/helper.ts
+++ u2u-network/src/helper.ts
@@ -6,7 +6,8 @@ export const STAKING_ADDRESS = Address.fromString('0xfc00face0000000000000000000
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
 export let TWO_BI = BigInt.fromI32(2)
-export let HUNDRED_BI = BigInt.fromI32(10000)
+export let HUNDRED_BI = BigInt.fromI32(100)
+export let FIVE_BI = BigInt.fromI32(5)
 
 export let DECIMAL_BI = BigInt.fromI64(1000000000000000000)
 
--- u2u-network/src/network.ts
+++ u2u-network/src/network.ts
@@ -2,9 +2,9 @@ import { ethereum, log, BigInt } from "@graphprotocol/graph-ts"
 import {
   SFC
 } from "../generated/SFC/SFC"
-import { DECIMAL_BI, ONE_BI, STAKING_ADDRESS, ZERO_BI, concatID } from "./helper"
-import { newEpoch, newValidator } from "./initialize"
-import { Epoch, Validator } from "../generated/schema"
+import { DECIMAL_BI, FIVE_BI, HUNDRED_BI, ONE_BI, STAKING_ADDRESS, ZERO_BI, concatID } from "./helper"
+import { loadNetwork, newBlock, newEpoch, newValidator } from "./initialize"
+import { Block as BlockEntity, Epoch, Validator } from "../generated/schema"
 
 
 export function handleBlockWithCallToContract(block: ethereum.Block): void { }
@@ -18,12 +18,19 @@ export function handleBlock(block: ethereum.Block): void {
     return;
   }
   let currentEpochID = currenEpochResult.value
-  if(currentEpochID.isZero()) {
+  if (currentEpochID.isZero()) {
     log.error("Epoch zero", [])
     return;
   }
+  // Update block
+  updateBlock(block, currentEpochID)
   let _lastEpoch = currentEpochID.minus(ONE_BI)
   log.info("Epoch handle with currentEpoch: {}, lastEpoch: {}", [currentEpochID.toString(), _lastEpoch.toString()])
+  updateEpoch(_lastEpoch, block, stakingSMC)
+  updateValidators(_lastEpoch)
+}
+
+function updateEpoch(_lastEpoch: BigInt, block: ethereum.Block, stakingSMC: SFC): void {
   let epochEntity = Epoch.load(_lastEpoch.toHexString())
   if (epochEntity != null) {
     return
@@ -36,7 +43,9 @@ export function handleBlock(block: ethereum.Block): void {
     log.error("get epochSnapshotResult reverted", [])
     return
   }
-  let epochSnapshot= epochSnapshotResult.value
+  let network = loadNetwork()
+  network.lastEpoch = _lastEpoch
+  let epochSnapshot = epochSnapshotResult.value
   epochEntity.endTime = epochSnapshot.getEndTime()
   epochEntity.totalBaseReward = epochSnapshot.getTotalBaseRewardWeight()
   epochEntity.totalTxReward = epochSnapshot.getTotalTxRewardWeight()
@@ -45,38 +54,74 @@ export function handleBlock(block: ethereum.Block): void {
   epochEntity.rewardPerSecond = epochSnapshot.getBaseRewardPerSecond()
   epochEntity.epochFee = epochSnapshot.getEpochFee()
   let _epochRewards = ZERO_BI
-  if (_lastEpoch.gt(ONE_BI)) {
-    let _prevEpoch = Epoch.load(_lastEpoch.minus(ONE_BI).toHexString())
-    if (_prevEpoch != null) {
-      let _epochTimeSeconds = ZERO_BI
-      if (epochSnapshot.getEndTime().gt(_prevEpoch.endTime)) {
-        _epochTimeSeconds = epochSnapshot.getEndTime().minus(_prevEpoch.endTime)
-      }
-      _epochRewards = _epochTimeSeconds.times(epochSnapshot.getBaseRewardPerSecond()).plus(epochSnapshot.getEpochFee())
-      epochEntity.epochRewards = _epochRewards
-      epochEntity.totalRewards = _prevEpoch.totalRewards.plus(_epochRewards)
+  if (network.lastEpochEndTime.gt(ZERO_BI)) {
+    let _epochTimeSeconds = ZERO_BI
+    if (epochSnapshot.getEndTime().gt(network.lastEpochEndTime)) {
+      _epochTimeSeconds = epochSnapshot.getEndTime().minus(network.lastEpochEndTime)
+    }
+    _epochRewards = _epochTimeSeconds.times(epochSnapshot.getBaseRewardPerSecond()).plus(epochSnapshot.getEpochFee())
+    epochEntity.epochRewards = _epochRewards
+    epochEntity.totalRewards = network.totalRewards.plus(_epochRewards)
+    network.totalRewards = network.totalRewards.plus(_epochRewards)
+
+    let lastBlockEpoch = BlockEntity.load(network.lastEpochBlock.plus(ONE_BI).toString())
+    let currentBlockEpoch = BlockEntity.load(block.number.minus(ONE_BI).toString())
+
+    if (lastBlockEpoch != null && currentBlockEpoch != null) {
+      epochEntity.epochBurntFees = currentBlockEpoch.totalBurntFees.minus(lastBlockEpoch.totalBurntFees)
     }
   }
+  network.lastEpochEndTime = epochSnapshot.getEndTime()
+  network.lastEpochBlock = block.number.minus(ONE_BI)
+  network.save()
   epochEntity.save()
-  updateValidators(_lastEpoch)
 }
 
-function updateValidators (epochId: BigInt): void {
-    let _validators = getEpochValidators(epochId)
-    if (_validators.length == 0) {
-      log.error("get getEpochValidators empty", [])
-      return;
+function updateBlock(block: ethereum.Block, currentEpoch: BigInt): void {
+  let blockEntity = newBlock(block.number.toString())
+  blockEntity.epoch = currentEpoch
+  blockEntity.blockNumber = block.number
+  blockEntity.gasUsed = block.gasUsed
+  blockEntity.baseFeePerGas = block.baseFeePerGas
+  blockEntity.timestamp = block.timestamp
+  let network = loadNetwork()
+  let _bassFee = block.baseFeePerGas !== null ? block.baseFeePerGas : ZERO_BI
+  if (_bassFee) {
+    let _burntFees = _bassFee.times(block.gasUsed).times(HUNDRED_BI).div(FIVE_BI)
+    blockEntity.burntFees = _burntFees
+    if (block.number.gt(ZERO_BI)) {
+      let lastBlock = BlockEntity.load(block.number.minus(ONE_BI).toString())
+      if (lastBlock != null) {
+        let lastBurntFees = lastBlock.totalBurntFees
+        blockEntity.totalBurntFees = lastBurntFees.plus(_burntFees)
+        network.totalBurntFees = network.totalBurntFees.plus(_burntFees)
+      }
+    } else {
+      blockEntity.totalBurntFees = _burntFees
+      network.totalBurntFees = _burntFees
     }
-    for(let i = 0; i < _validators.length; ++i) {
-      _updateValidator(epochId, _validators[i])
-    }
+  }
+  network.lastBlock = block.number
+  network.save()
+  blockEntity.save()
 }
 
-function _updateValidator (epochId: BigInt, validatorId: BigInt): void {
+function updateValidators(epochId: BigInt): void {
+  let _validators = getEpochValidators(epochId)
+  if (_validators.length == 0) {
+    log.error("get getEpochValidators empty", [])
+    return;
+  }
+  for (let i = 0; i < _validators.length; ++i) {
+    _updateValidator(epochId, _validators[i])
+  }
+}
+
+function _updateValidator(epochId: BigInt, validatorId: BigInt): void {
   let stakingSMC = SFC.bind(STAKING_ADDRESS)
   let _receivedStake = stakingSMC.try_getEpochReceivedStake(epochId, validatorId)
   let _accumulatedRewardPerToken = stakingSMC.try_getEpochAccumulatedRewardPerToken(epochId, validatorId)
-  let _entityId  = concatID(epochId.toHexString(), validatorId.toHexString())
+  let _entityId = concatID(epochId.toHexString(), validatorId.toHexString())
   let valEntity = Validator.load(_entityId)
   if (valEntity == null) {
     valEntity = newValidator(_entityId)
@@ -87,7 +132,7 @@ function _updateValidator (epochId: BigInt, validatorId: BigInt): void {
   valEntity.accumulatedRewardPerToken = _accumulatedRewardPerToken.value
   let _totalEpochRewards = _receivedStake.value.times(_accumulatedRewardPerToken.value).div(DECIMAL_BI)
   if (epochId.gt(ONE_BI)) {
-    let _prevId =  concatID((epochId.minus(ONE_BI)).toHexString(), validatorId.toHexString())
+    let _prevId = concatID((epochId.minus(ONE_BI)).toHexString(), validatorId.toHexString())
     let _prevEpoch = Validator.load(_prevId)
     if (_prevEpoch != null) {
       let _prevTotalEpochRewards = _prevEpoch.totalRewards
@@ -107,7 +152,7 @@ function _updateValidator (epochId: BigInt, validatorId: BigInt): void {
   valEntity.save()
 }
 
-function getEpochValidators(epochId: BigInt) : BigInt[] {
+function getEpochValidators(epochId: BigInt): BigInt[] {
   let stakingSMC = SFC.bind(STAKING_ADDRESS)
   let validators = stakingSMC.try_getEpochValidatorIDs(epochId)
   if (validators.reverted) {
@@ -116,3 +161,4 @@ function getEpochValidators(epochId: BigInt) : BigInt[] {
   }
   return validators.value
 }
+
