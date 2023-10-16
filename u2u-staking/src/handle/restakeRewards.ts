6490f6529e6601e0d63db727e28143e22bb14b4c
phidangnguyen <phidang0210@gmail.com>
2023-10-16 15:55:03 +0700

    [Fix] handle locked amount when restake

--- u2u-staking/src/handle/restakeRewards.ts
+++ u2u-staking/src/handle/restakeRewards.ts
@@ -1,8 +1,8 @@
 import { log, BigInt } from "@graphprotocol/graph-ts"
 import { RestakedRewards } from "../../generated/SFC/SFC"
 import { TransactionType, concatID } from "../helper"
-import { loadStaking, loadValidator, newTransaction } from "../initialize"
-import { Delegation, Delegator } from "../../generated/schema"
+import { loadStaking, loadValidator, newLockedUp, newTransaction } from "../initialize"
+import { Delegation, Delegator, LockedUp, Validation } from "../../generated/schema"
 
 /**
  * Handle restake rewards
@@ -10,16 +10,42 @@ import { Delegation, Delegator } from "../../generated/schema"
  */
 export function restakRewards(e: RestakedRewards): void {
   log.info("Restake rewards handle with txHash: {}", [e.transaction.hash.toHexString()])
+  let _lockedupId = concatID(e.params.toValidatorID.toHexString(), e.params.delegator.toHexString())
   const _totalRewards = e.params.lockupBaseReward.plus(e.params.lockupExtraReward).plus(e.params.unlockedReward)
+  const _lockupReward = e.params.lockupBaseReward.plus(e.params.lockupExtraReward)
   let _delegationId = concatID(e.params.toValidatorID.toHexString(), e.params.delegator.toHexString())
-  transactionUpdate(e, _totalRewards)
-  delegationUpdate(e, _delegationId, _totalRewards)
-  validatorUpdate(e, _totalRewards)
-  delegatorUpdate(e, _totalRewards)
+  let _validationId = concatID(e.params.delegator.toHexString(), e.params.toValidatorID.toHexString())
+  transactionUpdate(e, _totalRewards, _lockupReward)
+  delegationUpdate(e, _delegationId, _totalRewards, _lockedupId)
+  validatorUpdate(e, _totalRewards, _lockupReward)
+  delegatorUpdate(e, _totalRewards, _lockupReward)
+  lockedupUpdate(e, _lockedupId, _lockupReward)
+  validationUpdate(e, _validationId, _lockupReward)
 }
 
+function lockedupUpdate (e: RestakedRewards, _lockedupId: string, _lockupReward: BigInt): void {
+  // Lockedup load
+  let lockedup = LockedUp.load(_lockedupId)
+  if (lockedup == null) {
+    lockedup = newLockedUp(_lockedupId)
+    lockedup.delegator = e.params.delegator.toHexString()
+    lockedup.validator = e.params.toValidatorID.toHexString()
+  }
+  lockedup.lockedAmount = lockedup.lockedAmount.plus(_lockupReward)
+  lockedup.save()
+}
 
-function delegatorUpdate(e: RestakedRewards, totalRewards: BigInt): void {
+function validationUpdate(e: RestakedRewards, _validationId: string, _lockupReward: BigInt): void {
+  let validation = Validation.load(_validationId)
+  if (validation == null) {
+    log.error("Restake: load validation failed with ID: {}, txHash: {}", [_validationId, e.transaction.hash.toHexString()])
+    return
+  }
+  validation.totalLockStake = validation.totalLockStake.plus(_lockupReward)
+  validation.save()
+}
+
+function delegatorUpdate(e: RestakedRewards, totalRewards: BigInt, _lockupReward: BigInt): void {
   // Delegator table update
   let delegator = Delegator.load(e.params.delegator.toHexString())
   if (delegator == null) {
@@ -27,11 +53,12 @@ function delegatorUpdate(e: RestakedRewards, totalRewards: BigInt): void {
     return
   }
   delegator.totalClaimedRewards =  delegator.totalClaimedRewards.plus(totalRewards)
+  delegator.totalLockStake = delegator.totalLockStake.plus(_lockupReward)
   delegator.save()
 }
 
 
-function validatorUpdate(e: RestakedRewards, totalRewards: BigInt): void {
+function validatorUpdate(e: RestakedRewards, totalRewards: BigInt, _lockupReward: BigInt): void {
   let staking = loadStaking() // load staking
   let validator = loadValidator(e.params.toValidatorID.toHexString(), e.transaction.hash.toHexString())
   if (validator == null) {
@@ -42,22 +69,25 @@ function validatorUpdate(e: RestakedRewards, totalRewards: BigInt): void {
   validator.totalStakedAmount = _newTotalValStaked
   validator.totalClaimedRewards = validator.totalClaimedRewards.plus(totalRewards)
   staking.totalClaimedRewards = staking.totalClaimedRewards.plus(totalRewards)
+  validator.totalLockStake = validator.totalLockStake.plus(_lockupReward)
+  staking.totalLockStake = staking.totalLockStake.plus(_lockupReward)
 
   staking.save()
   validator.save()
 }
 
-function delegationUpdate(e: RestakedRewards, _delegationId: string, _totalRewards: BigInt): void {
+function delegationUpdate(e: RestakedRewards, _delegationId: string, _totalRewards: BigInt, _lockedupId: string): void {
   let delegation = Delegation.load(_delegationId)
   if (delegation == null) {
     log.error("Restake rewards: load delegation failed with ID: {}, txHash: {}", [e.params.toValidatorID.toHexString(), e.transaction.hash.toHexString()])
     return
   }
   delegation.totalClaimedRewards = delegation.totalClaimedRewards.plus(_totalRewards)
+  delegation.lockedUp = _lockedupId
   delegation.save()
 }
 
-function transactionUpdate(e: RestakedRewards, _totalRewards: BigInt): void {
+function transactionUpdate(e: RestakedRewards, _totalRewards: BigInt, _lockupReward: BigInt): void {
   let _transactionId = concatID(e.transaction.hash.toHexString(), TransactionType.Restake.toString())
   let transaction = newTransaction(_transactionId)
   transaction.txHash = e.transaction.hash
@@ -70,5 +100,6 @@ function transactionUpdate(e: RestakedRewards, _totalRewards: BigInt): void {
   transaction.block = e.block.number
   transaction.claimedAmount = _totalRewards
   transaction.stakedAmount = _totalRewards
+  transaction.lockedAmount = _lockupReward
   transaction.save()
 }
